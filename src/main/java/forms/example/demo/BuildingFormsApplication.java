3f21b40fbae5a18e6d1cfba50a910119929b33a3
Daniel Bonaparte <abonaparte3@yahoo.com>
2017-06-21 15:07:53 -0400

    first commit

---
+++ src/main/java/forms/example/demo/BuildingFormsApplication.java
@@ -0,0 +1,12 @@
+package forms.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuildingFormsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuildingFormsApplication.class, args);
+	}
+}
